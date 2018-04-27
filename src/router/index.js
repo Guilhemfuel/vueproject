aeee5fb1bba466f55ff1de2302669691448f03b6
Guilhem <guilhem.canivet@gmail.com>
2018-04-27 18:45:50 +0200

    Mode history + htaccess prod

--- src/router/index.js
+++ src/router/index.js
@@ -6,7 +6,7 @@ import Game from '@/components/Game'
 Vue.use(Router)
 
 export default new Router({
-  mode: 'hash',
+  mode: 'history',
   routes: [
     { path: '/', name: 'Home', component: Home },
     { path: '/:code', name: 'Game', component: Game }
